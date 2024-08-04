995f5dce73d614eb157a6ff5b402ffcd588fc890
Dulcie Jackson <dulciejackson@gmail.com>
2024-08-04 11:54:21 +0100

    :sparkles: Connects up user avatars to post filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useMemo, useState } from 'react';
 import { UserAvatar } from './components/UserAvatar';
 import useGetPosts from './hooks/useGetPosts'
 import useGetUsers from './hooks/useGetUsers';
+import { User } from './data/user';
 
 function App() {
-  const {data: users} = useGetUsers();
+  const {data: users, isLoading} = useGetUsers();
   const {data: posts} = useGetPosts();
+  const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
 
-  console.log(posts);
+  // Filter the visible post list down to only those authored by selected users
+  const filteredPosts = useMemo(() => posts?.filter(post => !!selectedUsers?.find(user => user.id === post.userId)), [selectedUsers, posts]);
+
+  useEffect(() => {
+    if (users && !isLoading) {
+      setSelectedUsers(users);
+    }
+  }, [users, isLoading]);
+
+  const updateSelectedUsers = (user: User) => {
+    // If the user is currently in the selected users, remove it
+    if (selectedUsers?.includes(user)) {
+      setSelectedUsers(selectedUsers.filter(selectedUser => selectedUser.id !== user.id));
+    // And if they're not selected yet, add them
+    } else {
+      setSelectedUsers([...selectedUsers, user])
+    }
+  }
 
   return (
     <div className='flex flex-col items-center justify-center'>
@@ -14,20 +34,20 @@ function App() {
         <h1 className='text-lg font-bold'>Posts</h1>
       </div>
       <div className='flex flex-row items-center gap-4 overflow-scroll w-full px-8 py-2'>
-        <div className='flex flex-col items-center'>
-          <div className='h-16 aspect-square rounded-full bg-gray-500'>
-          </div>
-          <p className='text-xs'>All</p>
-        </div>
         {
           users?.map(user => (
-            <UserAvatar user={user} />
+            <UserAvatar 
+              key={user.id}
+              user={user} 
+              isSelected={!!selectedUsers?.includes(user)} 
+              onClick={() => updateSelectedUsers(user)} 
+            />
           ))
         }
       </div>
       <div className='flex flex-col gap-6 mt-4 m-8'>
         {
-          posts?.map(post => {
+          filteredPosts?.map(post => {
             const postUser = users?.find(user => user.id === post.userId);
             return (
               <article className='p-6 border border-gray-500 rounded-md w-full'>
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -2,14 +2,20 @@ import { User } from "../data/user"
 
 interface UserAvatarProps {
     user: User;
+    isSelected: boolean;
+    onClick: () => void;
 }
 
-export const UserAvatar: React.FC<UserAvatarProps> = ({user}) => {
+export const UserAvatar: React.FC<UserAvatarProps> = ({user, isSelected, onClick}) => {
     return (
-        <div className='flex flex-col items-center'>
-            <div className='h-16 aspect-square rounded-full bg-gray-500'>
+        <button className='flex flex-col items-center' onClick={onClick}>
+            <div className={`h-16 aspect-square rounded-full overflow-hidden border-4 border-white outline outline-2 ${isSelected ? "outline-blue-500" : "outline-gray-300"}`}>
+                <img 
+                    src={`https://picsum.photos/seed/${user.username}/200`} 
+                    alt={`Profile photo for user ${user.username}`} 
+                />
             </div>
-            <p className='text-xs'>{user.username.toLowerCase()}</p>
-        </div>
+            <p className='mt-2 text-xs'>{user.username.toLowerCase()}</p>
+        </button>
     )
 }
